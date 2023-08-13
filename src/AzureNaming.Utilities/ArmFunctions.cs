bf68a63070ee7049ae3427fbee572c6e4bdb6b21
emmanuel <emmanuel@devopsabcs.com>
2023-08-13 13:52:28 -0400

    added comments

--- src/AzureNaming.Utilities/ArmFunctions.cs
+++ src/AzureNaming.Utilities/ArmFunctions.cs
@@ -3,6 +3,8 @@ using Newtonsoft.Json.Linq;
 
 namespace AzureNaming.Utilities
 {
+    //https://samcogan.com/using-bicep-functions-in-c-if-you-really-want-to/
+    //https://stackoverflow.com/questions/43295720/azure-arm-uniquestring-function-mimic/67399362#67399362
     public static class ArmFunctions
     {
         public static string? UniqueString(params string[] values)
